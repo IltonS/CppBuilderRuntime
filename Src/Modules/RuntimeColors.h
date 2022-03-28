d028e6a81708695f689862379bd22625a456ed53
IltonS <ilton.sequeira.s@gmail.com>
2022-03-28 16:41:18 -0300

    Reset project

--- Src/Modules/RuntimeColors.h
+++
@@ -1,9 +0,0 @@
-#ifndef RuntimeColorsH
-#define RuntimeColrsH
-
-const TColor MAIN_COLOR = 0x00BE6E10;
-const TColor MAIN_COLOR_10P_DARK = 0xAB630E;
-const TColor HOT_TRACK_COLOR = 0xFCF6EF;
-
-
-#endif
